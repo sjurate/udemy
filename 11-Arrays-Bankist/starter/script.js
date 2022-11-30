484cb55e8d576080125e384c297f6e9407fa775b
sjurate <rasaujuratei@gmail.com>
2022-11-30 16:20:37 +0200

    simple array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,38 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// SLICE
+// doesn't change original arr
+// first - start param, second- end param. Returns new array, doesn't change original
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2, 4));
+console.log(arr.slice(1, -2));
+
+// SPLICE
+// changes original arr. Removes from it the extracted elements
+// first - start param, second - the count(kiekis) of elements we want to remove
+
+arr.splice(-1);
+console.log(arr);
+
+arr = ['a', 'b', 'c', 'd', 'e'];
+
+// REVERSE
+// changes original arr!
+
+let arr2 = ['j', 'i', 'h', 'g', 'f'];
+
+console.log(arr2.reverse());
+
+// CONCAT
+// param - array that we want to add
+
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// JOIN
+
+console.log(letters.join('-'));
