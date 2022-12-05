8f551d256bb06f9f8e037c016fff435ac92f4fd4
sjurate <rasaujuratei@gmail.com>
2022-12-05 16:03:10 +0200

    sticky navigation primitive way

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -123,6 +123,20 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// STICKY NAVIGATION
+
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function (e) {
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
+
+// INTERS
+
 // TABBED COMPONENT
 
 const tabs = document.querySelectorAll('.operations__tab');
