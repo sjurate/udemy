e69b3b3caddf2a7249136f903f2b485b07cc68a8
sjurate <rasaujuratei@gmail.com>
2022-12-06 10:22:16 +0200

    script loading differences

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -509,3 +509,18 @@ const h1 = document.querySelector('h1');
 // [...h1.parentElement.children].forEach(function (el) {
 //   if (el !== h1) el.style.transform = 'scale(0.5)';
 // });
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded');
+});
+
+// To create a confimation message before leaving a page
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
