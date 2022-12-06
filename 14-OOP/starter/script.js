1573f063727a598576d38a4668559b88c20e5192
sjurate <rasaujuratei@gmail.com>
2022-12-06 14:14:25 +0200

    coding challange

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -60,3 +60,53 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+// Object.prototype
+console.log(jonas.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 7, 6, 7, 5, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+// To create a new method for an array
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+
+console.log(x => x + 1);
+
+// Coding challange
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+
+Car.prototype.break = function () {
+  this.speed = this.speed - 10;
+  console.log(this.speed);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+console.log(car1, car2);
+
+car1.accelerate();
+car1.break();
+console.log(car1);
+
+// ES6 CLASSES
