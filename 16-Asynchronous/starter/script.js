a88d58dc00accf0cf84830c1ec64e52742b63de1
sjurate <rasaujuratei@gmail.com>
2022-12-09 18:54:11 +0200

    testing event loop

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -163,8 +163,84 @@ const getCountryData = function (country) {
 //     });
 // };
 
-btn.addEventListener('click', function () {
-  getCountryData('portugal');
-});
+// btn.addEventListener('click', function () {
+//   getCountryData('portugal');
+// });
 
-getCountryData('australia');
+// getCountryData('australia');
+
+// const getCountryData = function (country) {
+//   // Country 1
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     // then() accepts 2 functions - 1st for succes case, 2nd - for error case (catching error/handling error)
+//     .then(response => {
+//       console.log(response);
+//       if (!response.ok) throw new Error(`Country not found ${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+//       if (!neighbour) return;
+//       // Country 2
+//       // the value which is returned will be a succuess value of the returned promise
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok) throw new Error(`Country not found ${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     // catch method in the end of chain catches ALL errors in the chain, then we don't need to pass a second error function in .then() method
+//     .catch(err => {
+//       console.error(`${err} `);
+//       renderError(`Something went wrong... ${err.message}`);
+//     })
+//     // will always be called - no matter if promise is fullfiled or rejected
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+// Testing event loop ------
+
+// console.log('Test start');
+
+// setTimeout(() => console.log('0 sec timer'), 0);
+
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// console.log('Test end');
+
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let i = 0; i < 10000000; i++) {
+//     i++;
+//   }
+//   console.log(res);
+// });
