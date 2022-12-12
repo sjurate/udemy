9237f2e64d07bddfc05aac558f8ecbc3b3038c2d
sjurate <rasaujuratei@gmail.com>
2022-12-12 20:01:05 +0200

    modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -16,3 +16,26 @@ console.log('Inporting module');
 import add from './shoppingCart.js';
 
 add('pizza', 2);
+
+// TOP LEVEL AWAIT
+
+// async function x() {
+//     //await ....
+// }
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// not very clean
+// lastPost.then(last => console.clear(last));
+
+const lastPost = await getLastPost();
+console.log(lastPost);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,13 @@
 // exporting module
 console.log('Exporting module');
 
+// Blocking code
+// console.log('Start fetching users:');
+
+// await fetch('https://jsonplaceholder.typicode.com/users');
+
+// console.log('Finish fetching users.');
+
 const shippingCost = 10;
 const cart = [];
 
