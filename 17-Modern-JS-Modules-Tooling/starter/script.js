23e218a4409e131b34c595b6c606cd5c5556045e
sjurate <rasaujuratei@gmail.com>
2022-12-12 21:05:31 +0200

    deep clone using lodash library

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -39,3 +39,56 @@ const getLastPost = async function () {
 
 const lastPost = await getLastPost();
 console.log(lastPost);
+
+// Module pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (prod, quantity) {
+    cart.push({ prod, quantity });
+    console.log(`${quantity} ${prod} was added to cart`);
+  };
+  const orderStock = function (prod, quantity) {
+    console.log(`${quantity} ${prod} ordered from supplier `);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4); // addToCart function can still access cart because it's scope is the same as this function's scope (the function's birth place scope), so function can still manipulate it
+console.log(ShoppingCart2);
+
+import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    {
+      product: 'bread',
+      quantity: 5,
+    },
+    {
+      product: 'pizza',
+      quantity: 5,
+    },
+  ],
+  user: {
+    loggedIn: true,
+  },
+};
+
+const stateClone = Object.assign({}, state);
+
+console.log(stateClone);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+
+console.log(stateDeepClone);
