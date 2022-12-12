19277c11798859f810966323bd6380e07cc547e0
sjurate <rasaujuratei@gmail.com>
2022-12-12 10:42:47 +0200

    coding challenge - promisifying

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -296,6 +296,12 @@ Promise.reject('abc').catch(res => console.error(res));
 
 // navigator.geolocation.getCurrentPosition(success(position), error(err));
 
+// const wait = function (sec) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, sec * 1000);
+//   });
+// };
+
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     // navigator.geolocation.getCurrentPosition(
@@ -307,3 +313,42 @@ const getPosition = function () {
 };
 
 getPosition().then(position => console.log(position));
+
+// Challenge
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Img 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Img 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
