77f3e0092393fc3b9a394982c9171f1194ef7bc2
sjurate <rasaujuratei@gmail.com>
2022-12-12 13:05:44 +0200

    Promise.race() - returns first settled promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -454,3 +454,29 @@ const get3countries = async function (c1, c2, c3) {
 };
 
 get3countries('portugal', 'canada', 'tanzania');
+
+// Promise.race() - takes an array of promises too, returns first settled promise (no matter if it's fulfilled or rejected)
+
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/tanzania`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
