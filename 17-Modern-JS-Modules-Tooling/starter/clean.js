756b9628ff676c2bd24584038011252d2908aff0
sjurate <rasaujuratei@gmail.com>
2022-12-13 17:09:00 +0200

    some pure functions examples

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,7 @@
-const budget = [
+'strict mode';
+
+// Object.freeze makes the object imutable (can't change it)
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,51 +10,99 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+budget[0].value = 10000;
+
+// Object.freeze makes the object imutable (can't change it)
+
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+// const getLimit = user => {
+//   // const limit = spendingLimits?.[user] ??  0;
+//   spendingLimits[user] ? spendingLimits[user] : 0;
+// };
+
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+// const addExpense = function (
+//   state,
+//   limits,
+//   value,
+//   description,
+//   user = 'jonas'
+// ) {
+//   const cleanUser = user.toLowerCase();
+//   console.log(cleanUser);
+
+//   return value <= getLimit(cleanUser)
+//     ? [...state, { value: -value, description, user: cleanUser }]
+//     : state;
+// };
+
+// Pure function :D
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
 
-const getLimit = user => {
-  spendingLimits[user] ? spendingLimits[user] : 0;
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+
+console.log(newBudget1);
+console.log(newBudget2);
+console.log(newBudget3);
+
+const checkExpenses = function (state, limits) {
+  // for (const entry of newBudget3) {
+  //   if (entry.value < -getLimit(limits, entry.user)) {
+  //     entry.flag = 'limit';
+  //   }
+  // }
+  return state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : { ...entry }
+  );
 };
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
-  const limit = getLimit(user);
-  // const limit = spendingLimits?.[user] ??  0;
-  if (value <= limit) {
-    budget.push({ value: -value, description, user });
-  }
-};
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
-console.log(budget);
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
 
-const checkExpenses = function () {
-  for (const entry of budget) {
-    if (entry.value < -getLimit(entry.user)) {
-      entry.flag = 'limit';
-    }
-  }
+const logBigExpenses = function (state, bigLimit) {
+  // let output = '';
+  // for (const entry of budget) {
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+  // }
+
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
+  const bigExpenses = state
+    .filter(expense => expense.value <= -bigLimit)
+    .map(expense => expense.description.slice(-2))
+    .join(' / ');
+  console.log(bigExpenses);
 };
 
-checkExpenses();
-
-console.log(budget);
-
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget) {
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
-  }
-
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-
-logBigExpenses(500);
+logBigExpenses(finalBudget, 500);
